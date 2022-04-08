7710f75c83eac8878a1d14cde5781b37ab6e96a3
jpiedrafita <4170476+jpiedrafita@users.noreply.github.com>
2022-04-08 09:40:09 +0200

    terminal

--- scripts/centos7install.sh
+++ scripts/centos7install.sh
@@ -6,7 +6,8 @@ yum update
 yum install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
 
 # Install yum utilities
-yum update && yum install -y tree git python3
+yum update && yum install -y tree git python3 neofetch dnf
+#dnf -y install dnf-plugins-core
 
 # Update curl latest version using city-fan --> required for homebrew
 rpm -Uvh http://www.city-fan.org/ftp/contrib/yum-repo/rhel8/x86_64/city-fan.org-release-2-2.rhel8.noarch.rpm
@@ -29,11 +30,6 @@ alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 mkdir .zah && touch $HOME/.zsh/aliases
 echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zsh/aliases
 
-
-##############
-#### TODO ####
-##############
-
 # Install gnome tweaks
 yum install -y chrome-gnome-shell gnome-tweak-tool sassc intltool
 
@@ -41,3 +37,15 @@ yum install -y chrome-gnome-shell gnome-tweak-tool sassc intltool
 
 # Install conky
 yum install conky conky-manager
+
+# Terminal
+# Gnome nord terminal
+git clone https://github.com/arcticicestudio/nord-gnome-terminal.git
+source nord-gnome-terminal/src/nord.sh
+rm -rf nord-gnme-terminal
+
+# Oh-my-bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
