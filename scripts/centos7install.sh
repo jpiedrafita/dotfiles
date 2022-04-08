9a459292e55ae5c16dcb4873aade59addf6fa988
jpiedrafita <4170476+jpiedrafita@users.noreply.github.com>
2022-04-08 10:50:50 +0200

    final changes before 1st test

--- scripts/centos7install.sh
+++ scripts/centos7install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+###################
+###### Repos ######
+###################
+
 yum update
  
 # Add centos7 endpoint x86_64 repo
 yum install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
 
 # Install yum utilities
-yum update && yum install -y tree git python3 neofetch dnf
+yum update && yum install -y tree git python3 neofetch dnf snapd
+systemctl enable --now snapd.socket
 #dnf -y install dnf-plugins-core
 
 # Update curl latest version using city-fan --> required for homebrew
@@ -19,16 +24,12 @@ yum install -y curl
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# Install brew stuff
+###################
+#### Utilities ####
+###################
+
+# Install gh tools from homebrew
 brew install gh
-
-
-###############################
-
-# Create Alias for git .dotfiles
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-mkdir .zah && touch $HOME/.zsh/aliases
-echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zsh/aliases
 
 # Install gnome tweaks
 yum install -y chrome-gnome-shell gnome-tweak-tool sassc intltool
@@ -49,3 +50,42 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/mast
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+########################
+##### Applications #####
+########################
+
+# Install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum install -y ./google-chrome*
+rm ./google-chrome*
+
+# Install thunderbird
+snap install thunderbird
+
+##############################
+######## git dotfiles ########
+##############################
+
+# Create alias
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' > .bashrc
+# Exclude .dotfiles to avoid recursive problems
+echo ".dotfiles" > .gitignore
+# Clone the bare repo
+git clone --bare https://github.com/jpiedrafita/dotfiles.git $HOME/.dotfiles
+# Backup checkout conflict files
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+config checkout
+config config --local status.showUntrackedFiles no
+
+#############################
+##### Manual activities #####
+#############################
+
+# Login in github
+# gh auth login
+
+# Install firefox from: https://snapcraft.io/firefox
